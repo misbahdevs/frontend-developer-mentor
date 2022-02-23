050c77f59b0a404bf694e65b138b0f04d4de4ec4
misbahX <misbahx.id@gmail.com>
2022-02-23 23:46:26 +0700

    menambahkan file challenge pertama dari frontend developer mentor

---
+++ dashboard-admin/main.js
@@ -0,0 +1,40 @@
+// ================== AJAX ===================
+const xhttp = new XMLHttpRequest();
+let data = "";
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    data = JSON.parse(xhttp.responseText);
+    console.log(data);
+
+    // ============== VARIABLE =============
+    const lists = document.querySelectorAll(".list__items");
+    const title = document.querySelectorAll(".type");
+    const hours = document.querySelectorAll(".hours");
+    const description = document.querySelectorAll(".deskription");
+    lists.forEach(function (list, index) {
+      list.addEventListener("click", function (e) {
+        if (index == 0) {
+          for (let i = 0; i < data.length; i++) {
+            title[i].innerHTML = data[i].title;
+            hours[i].innerHTML = data[i].timeframes.daily.current + "Hrs";
+            description[i].innerHTML = "Last Week - " + data[i].timeframes.daily.previous + "Hrs";
+          }
+        } else if (index == 1) {
+          for (let i = 0; i < data.length; i++) {
+            title[i].innerHTML = data[i].title;
+            hours[i].innerHTML = data[i].timeframes.weekly.current + "Hrs";
+            description[i].innerHTML = "Last Week - " + data[i].timeframes.weekly.previous + "Hrs";
+          }
+        } else if (index == 2) {
+          for (let i = 0; i < data.length; i++) {
+            title[i].innerHTML = data[i].title;
+            hours[i].innerHTML = data[i].timeframes.monthly.current + "Hrs";
+            description[i].innerHTML = "Last Week - " + data[i].timeframes.monthly.previous + "Hrs";
+          }
+        }
+      });
+    });
+  }
+};
+xhttp.open("GET", "data.json", true);
+xhttp.send();
